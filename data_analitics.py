3aad426209d3dfda7de9332b978b72513d81b8a8
DaGree <mr.dan6886@yandex.ru>
2021-09-16 17:28:22 +0300

    parsing

--- data_analitics.py
+++ data_analitics.py
@@ -1,10 +1,8 @@
 import apriori_python  #library for analys
 import csv #library for usin csv-files
 
-i = 0
 with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
-    reader = csv.reader(File)
-    for row in reader:
-        while i < 10:
-            print(row)
-            i=i+1    
+    reader = csv.reader(File,delimiter=',')
+    for row in reader:  
+        print(row[3]) 
+    print(row[3])
