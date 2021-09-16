3c72296a7cc48b82a6fdd784468505e63cde593d
DaGree <mr.dan6886@yandex.ru>
2021-09-16 17:46:57 +0300

    Copy dataset to array

--- data_analitics.py
+++ data_analitics.py
@@ -1,8 +1,15 @@
 import apriori_python  #library for analys
 import csv #library for usin csv-files
 
+
+assets=[]
 with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
     reader = csv.reader(File,delimiter=',')
     for row in reader:  
-        print(row[3]) 
-    print(row[3])
+        assets.append(row[3])
+    
+i = 1    
+while i <= 10:
+    print(i,' ',assets[i], ' ')
+    #print()
+    i=i+1
