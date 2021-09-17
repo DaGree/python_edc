2167b80258c5d4b75f7c577702c2463b578742e9
DaGree <mr.dan6886@yandex.ru>
2021-09-17 14:17:20 +0300

    adding new dataset

--- data_analitics.py
+++ data_analitics.py
@@ -1,15 +1,21 @@
-import apriori_python  #library for analys
-import csv #library for usin csv-files
-
+import csv #library for us in csv-files
+from apriori_python import apriori #lib foor analys
 
 assets=[]
 with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
     reader = csv.reader(File,delimiter=',')
     for row in reader:  
-        assets.append(row[3])
-    
-i = 1    
-while i <= 10:
-    print(i,' ',assets[i], ' ')
-    #print()
-    i=i+1
+        assets.append(row)
+
+def data_parser(filename):
+    def data_gen():
+        with open(filename) as file:
+            for line in file:
+                yield tuple(k.strip() for k in line.split(','))
+    return data_gen
+
+transactions = data_parser('dataset\Basket.csv')
+
+freqItemSet, rules = apriori(transactions, minSup=0.4, minConf=0.5)
+print(rules)
+print(freqItemSet)
