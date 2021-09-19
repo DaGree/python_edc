8e0485fe4643057a2375c1a387872e881285f734
DaGree <mr.dan6886@yandex.ru>
2021-09-19 18:31:32 +0300

    fixing UnicodeErr

--- data_analitics.py
+++ data_analitics.py
@@ -1,21 +1,25 @@
 import csv #library for us in csv-files
 from apriori_python import apriori #lib foor analys
 
-assets=[]
-with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
+transactions=[]
+with open('dataset\Basket.csv', newline='', encoding='latin-1') as File:  
+    assets=[]
     reader = csv.reader(File,delimiter=',')
     for row in reader:  
-        assets.append(row)
+        assets.append(row[1])
 
 def data_parser(filename):
-    def data_gen():
+    
+    def data_par():
         with open(filename) as file:
             for line in file:
                 yield tuple(k.strip() for k in line.split(','))
-    return data_gen
-
-transactions = data_parser('dataset\Basket.csv')
-
-freqItemSet, rules = apriori(transactions, minSup=0.4, minConf=0.5)
+    return data_par
+    
+#transactions = list(data_parser('dataset\Basket.csv'))
+#print(type(transactions))
+for i in range(10):
+    print(assets[i])
+freqItemSet, rules = apriori(assets, minSup=0.3, minConf=0.3)
 print(rules)
 print(freqItemSet)
