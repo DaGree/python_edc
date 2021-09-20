ee35fd199a534126e65eac2a4bcbda5c2fc3b1b9
DaGree <mr.dan6886@yandex.ru>
2021-09-20 15:56:51 +0300

    just on more commit

--- data_analitics.py
+++ data_analitics.py
@@ -1,21 +1,23 @@
 import csv #library for us in csv-files
 from apriori_python import apriori #lib foor analys
 
-transactions=[]
-with open('dataset\BreadBasket_DMS.csv', newline='',) as File:  
+with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
     assets=[]
     reader = csv.reader(File,delimiter=',')
-    for row in reader: 
+    for row in reader:
         if row[2]!='Transaction':
             i = int(row[2])
-            assets.insert(i,row[3])
+            #assets.append(row[3])
+            assets.insert(i,assets.append(row[3]))
  
 #transactions = list(data_parser('dataset\Basket.csv'))
 #print(type(transactions))
+#print(assets)
 for i in range(len(assets)):
-    # будем теперь обходить массив z[i]
     for j in range(len(assets[i])):
+     
         print(assets[i][j])
+      
 #freqItemSet, rules = apriori(assets, minSup=0.3, minConf=0.3)
 #print(rules)
 #print(freqItemSet)
