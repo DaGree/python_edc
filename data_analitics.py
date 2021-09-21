004848bd494b00446b6e81b361cf5f9f1f28a6c8
DaGree <mr.dan6886@yandex.ru>
2021-09-21 11:27:59 +0300

    change to transaction

--- data_analitics.py
+++ data_analitics.py
@@ -1,23 +1,34 @@
 import csv #library for us in csv-files
 from apriori_python import apriori #lib foor analys
 
+def output(assets):
+    for i in range(9683):
+        for j in range(100):
+            print(assets[i][j])
+    
+def input_arr(reader):
+    A=[]
+    for i in range(int(reader)):
+        row = input().split()
+    for i in range(len(row)):
+        row[i] = int(row[i])
+        A.append(row)
+    return A
+
 with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
-    assets=[]
+    transactions=[]
     reader = csv.reader(File,delimiter=',')
     for row in reader:
         if row[2]!='Transaction':
             i = int(row[2])
-            #assets.append(row[3])
-            assets.insert(i,assets.append(row[3]))
+            #transactions.append(i)
+            #transactions.append(row[3])
+            transactions.insert(i,row[3])
  
 #transactions = list(data_parser('dataset\Basket.csv'))
 #print(type(transactions))
 #print(assets)
-for i in range(len(assets)):
-    for j in range(len(assets[i])):
-     
-        print(assets[i][j])
-      
-#freqItemSet, rules = apriori(assets, minSup=0.3, minConf=0.3)
-#print(rules)
-#print(freqItemSet)
+#output(assets)
+freqItemSet, rules = apriori(transactions, minSup=0.4, minConf=0.5)
+print(rules)
+print(freqItemSet)
