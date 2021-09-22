b4fac8683f93d32f00754ecc93fba63fc1d6cf6d
DaGree <mr.dan6886@yandex.ru>
2021-09-22 13:11:06 +0300

    defragmentation and parsing function

--- data_analitics.py
+++ data_analitics.py
@@ -1,34 +1,36 @@
 import csv #library for us in csv-files
 from apriori_python import apriori #lib foor analys
 
-def output(assets):
-    for i in range(9683):
-        for j in range(100):
-            print(assets[i][j])
-    
-def input_arr(reader):
-    A=[]
-    for i in range(int(reader)):
-        row = input().split()
-    for i in range(len(row)):
-        row[i] = int(row[i])
-        A.append(row)
-    return A
+def data_parser():
+    assets=[]
+    with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
+        reader = csv.reader(File,delimiter=',')
+        for row in reader:
+            i=0
+            j = int(row[2])-1
+            for i in range(j):
+                assets.append(row[3])      
+    return assets
 
-with open('dataset\BreadBasket_DMS.csv', newline='') as File:  
+def analys_apriori(datar):
+    freqItemSet, rules = apriori(datar, minSup=0.2, minConf=0.3)
+    print(rules)
+    print(freqItemSet)
+
+def output(a):
+    with open('dataset\BreadBasket.csv', mode="w", encoding='utf-8') as w_file:
+        writer = csv.DictWriter(w_file, delimiter = ",",lineterminator="\r")
+    for i in range (len(datar)):
+        for j in range (len(datar[i])):
+            print(datar[i][j])
+   
+with open('dataset\Basket.csv', newline='') as File:  
     transactions=[]
     reader = csv.reader(File,delimiter=',')
     for row in reader:
-        if row[2]!='Transaction':
-            i = int(row[2])
-            #transactions.append(i)
-            #transactions.append(row[3])
-            transactions.insert(i,row[3])
- 
-#transactions = list(data_parser('dataset\Basket.csv'))
-#print(type(transactions))
-#print(assets)
-#output(assets)
-freqItemSet, rules = apriori(transactions, minSup=0.4, minConf=0.5)
-print(rules)
-print(freqItemSet)
+        transactions.append(row)
+
+datar = []
+datar=data_parser()         
+#print(datar)
+#analys_apriori(datar)
