93ff35c19ce84b5f8658ecba8c77509c192d81c5
DaGree <mr.dan6886@yandex.ru>
2021-09-26 13:46:06 +0300

    function data_parser is working. create new  file

--- data_analitics.py
+++ data_analitics.py
@@ -1,8 +1,8 @@
 import csv #library for us in csv-files
 from apriori_python import apriori #lib foor analys
-
+import pandas as pd
 def analys_apriori(datar):
-    freqItemSet, rules = apriori(datar, minSup=0.2, minConf=0.3)
+    freqItemSet, rules = apriori(datar, minSup=0.9, minConf=0.9)
     print(rules)
     print(freqItemSet)
 
@@ -15,13 +15,16 @@ def data_parser(name):
             if j==int(row[2]):
                 assets.append(row[3])
             else:
-                print(assets)
+                with open('dataset\BreadBasket.csv', mode='a', newline='') as e_file:
+                    e_writer = csv.writer(e_file, delimiter=',')
+                    e_writer.writerow(assets) 
+                #print(assets)
                 j=int(row[2])
                 assets.clear()
                 assets.append(row[3])
+    e_file.close
     print('Writing complete')
               
-
 def data_reader(name):
     with open(name, newline='') as File:  
         transactions=[]
@@ -33,5 +36,6 @@ def data_reader(name):
 #datar = []
 #datar=data_reader('dataset\Basket.csv')     
 data_parser('dataset\BreadBasket_DMS.csv') 
+datar=data_reader('dataset\BreadBasket.csv')   
 #print(datar)
-#analys_apriori(datar)
+analys_apriori(datar)
