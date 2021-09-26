fd103ead42e5b8b9b5bf285dfc515cd02abe0e38
DaGree <mr.dan6886@yandex.ru>
2021-09-26 16:11:36 +0300

    add new analys function

--- data_analitics.py
+++ data_analitics.py
@@ -1,10 +1,28 @@
 import csv #library for us in csv-files
-from apriori_python import apriori #lib foor analys
+#from apriori_python import apriori #lib foor analys
+from efficient_apriori import apriori
+from fpgrowth_py import fpgrowth
 import pandas as pd
-def analys_apriori(datar):
-    freqItemSet, rules = apriori(datar, minSup=0.9, minConf=0.9)
+
+def analys_efficient_apriori(datar):
+    freqItemSet, rules = apriori(datar, min_support=0.01, min_confidence=0.01) #minSup=0.05, minConf=0.001 min_support=0.01, min_confidence=0.01
+    rules_rhs=filter(lambda rule: len(rule.lhs)==1 and len(rule.rhs)==1,rules)
+    for rule in sorted(rules_rhs, key=lambda rule: rule.confidence):
+        print(rule)
     print(rules)
+    print()
     print(freqItemSet)
+
+def analys_fpgrowth(datar):
+    freqItemSet, rules = fpgrowth(datar, minSupRatio=0.5, minConf=0.5)
+    print(rules)
+
+def analys_apriori(datar):
+    freqItemSet, rules = apriori(datar, minSup=0.05, minConf=0.001) #minSup=0.05, minConf=0.001 min_support=0.01, min_confidence=0.01
+    print(rules)
+    print()
+    print(freqItemSet)
+
 
 def data_parser(name):
     j=0 
@@ -35,7 +53,7 @@ def data_reader(name):
 
 #datar = []
 #datar=data_reader('dataset\Basket.csv')     
-data_parser('dataset\BreadBasket_DMS.csv') 
+#data_parser('dataset\BreadBasket_DMS.csv') 
 datar=data_reader('dataset\BreadBasket.csv')   
 #print(datar)
-analys_apriori(datar)
+analys_efficient_apriori(datar)
